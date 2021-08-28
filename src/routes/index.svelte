e1406fc0bd2455cd926390b2ef38bea98319da80
Ian LIM <mallim.ink@gmail.com>
2021-08-28 21:37:52 +0800

    Configured the site to run the Netlify CMS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,27 @@
+<script>
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		if (window.netlifyIdentity) {
+			window.netlifyIdentity.on("init", (user) => {
+				if (!user) {
+					window.netlifyIdentity.on("login", () => {
+						document.location.href = "/admin/";
+					});
+				}
+			});
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+	<script
+		src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 <p><a sveltekit:prefetch href="/blog">Click here</a> to see the blog</p>
